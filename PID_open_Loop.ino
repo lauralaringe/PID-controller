fe19441c10391ddace4704f884fb06ba52b19c8c
moe-idris <73137000+moe-idris@users.noreply.github.com>
2020-10-22 19:35:38 +0200

    Update PID_open_Loop.ino

--- PID_open_Loop.ino
+++ PID_open_Loop.ino
@@ -1,17 +1,17 @@
 /* Code to test the open loop system and write a value to the LED. */
 
-// Definition of the LDR measurmente (ADC A0) and the PWM digital output pin (9)
+// Definition of the LDR measurmente (ADC A0) and the PWM digital output pin (9) - LDR is connected to the analog side (A0), and LED is connected to the digital side (P9) //
 byte LEDpin = 9;
 byte LDRpin = A0;
 
-// Definition of auxiliar variables
+// Definition of auxiliar variables. The input and output of the control system is required to be defined //
 String lecture;
 double Input, Output;
 
-// Definition of time auxiliar variables
+// Definition of time auxiliar variables //
 unsigned long Time = 0, Time_ant = 0;
 
-// Function to read data from the keyboard
+// Function to read data from the keyboard //
 void newConfigParameter() {
 
   if (Serial.available() > 0) {
@@ -20,33 +20,33 @@ void newConfigParameter() {
   }
 }
 
-// Setup function to initialize the serial communications and the output pin
+// Setup function to initialize the serial communications and the output pin //
 void setup() {
   Serial.begin(9600);
   pinMode(LEDpin, OUTPUT);
 }
 
-// Loop function
+// Loop function //
 void loop() 
 {
   // Read the current time in miliseconds
   Time = millis();
     
-  // Read data from the keyboard
+  // Function to read data from the keyboard //
   if (Serial.available() > 0) {
     lecture = Serial.readString();
     Input = lecture.toFloat();
     analogWrite(LEDpin, Input);
   }
 
-  // Each second, read the LDP measurment and print
+  // Each second, read the LDP measurment and print //
   if (Time - Time_ant >= 1000)
   { 
       // Measurement of the LDR sensor, and scale the value from 1023 to 255
       // In the PWM 5V are equal to 255
       Output = map(analogRead(LDRpin), 0, 1023, 0.0, 255.0);
       
-      // Print data
+      // Function to Print data //
       //Serial.print("LED input: ");
       Serial.print(Input);
       Serial.print("; ");
